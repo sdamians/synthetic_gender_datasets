8e1b387fbc3c6597927c1d0d94fa6cb0308e6a13
Sergio Arturo Damian Sandoval <sergio.damian181091@gmail.com>
2025-11-17 17:59:25 -0600

    updating head_focused_or_diffuse func

--- src/experiments/interpretation.py
+++ src/experiments/interpretation.py
@@ -8,60 +8,59 @@ from transformer_lens import ActivationCache
 import numpy as np
 
 def head_focused_or_diffuse(heads: list[tuple[int,int]],
-                            q_pos:int,
+                            q_pos: Float[Tensor, "batch"],
                             cache: ActivationCache,
                             model: HookedTransformer,
-                            name_pos: int,
-                            verb_pos: int,
-                            last_pos: int,
-                            example_prompt_ids: list,
-                            topk_thresholds=(0.4,0.8)):
+                            name_pos: Float[Tensor, "batch"],
+                            verb_pos: Float[Tensor, "batch"],
+                            last_pos: Float[Tensor, "batch"],
+                            topk_threshold=0.4):
   results = {}
   for layer, head in heads:
-    attn_pattern = cache["pattern", layer][:, head, q_pos] # batch, seqK
+    org_attn_pattern = cache["pattern", layer][t.arange(q_pos.size(0)), head, q_pos] # batch, seqK
 
     #Removemos el token inicial, sumamos todos los valores del batch y posteriormente normalizamos
-    attn_pattern = attn_pattern[:, 1:].sum(dim=0)
-    attn_pattern = attn_pattern / attn_pattern.sum()
+    attn_pattern = org_attn_pattern[:, 1:]
+    # Sum without the first token
+    den = attn_pattern.sum(dim=-1, keepdim=True)
+    # Avoid division by zero
+    den = den + 1e-12
+    # Renormalize
+    attn_renorm = attn_pattern / den
 
-    seq_len = len(attn_pattern)
-    entropy_max = t.log(t.tensor(seq_len)).item()
+    # attn para name, verb, last
+    name_attn_pattern = attn_renorm[t.arange(name_pos.size(0)), name_pos-1] # batch
+    verb_attn_pattern = attn_renorm[t.arange(verb_pos.size(0)), verb_pos-1] # batch
+    last_attn_pattern = attn_renorm[t.arange(last_pos.size(0)), last_pos-1] # batch
 
-    # entropia alta >1.5 = el pattern es focused
-    entropy = -t.sum(attn_pattern * t.log(attn_pattern + 1e-12)).item()
-    entropy = entropy / entropy_max
-
+    positions = ["name", "verb", "last"]
+    attn = t.tensor([ name_attn_pattern.mean().item(), verb_attn_pattern.mean().item(), last_attn_pattern.mean().item() ])
+    
     # se extraen el top 2 tokens que más se les ponen atención
-    sorted_vals, sorted_idx = t.sort(attn_pattern, descending=True)
-    sorted_idx = sorted_idx + 1
+    sorted_vals, sorted_idx = t.sort(attn, descending=True)
+    
     top1_frac = sorted_vals[0].item()
     top2_frac = sorted_vals[:2].sum().item()
 
     # --- Clasificación ---
-    if top1_frac > topk_thresholds[0]:
+    if top1_frac > topk_threshold:
         tipo = "focused"
-        posiciones_relevantes = [sorted_idx[0].item()]
-        valores_relevantes = [sorted_vals[0].item()]
-    elif top2_frac > topk_thresholds[0]:
+        position = positions[sorted_idx[0]]
+
+    elif top2_frac > topk_threshold:
         tipo = "bi-focused"
-        posiciones_relevantes = sorted_idx[:2].tolist()
-        valores_relevantes = sorted_vals[:2].tolist()
+        position = f"{positions[sorted_idx[0]]}-{positions[sorted_idx[1]]}"
     else:
         tipo = "diffuse"
-        posiciones_relevantes = sorted_idx[:3].tolist()
-        valores_relevantes = sorted_vals[:3].tolist()
-
-    key_pos = { name_pos: "name", verb_pos: "verb", last_pos: "last" }
-    rel_pos = [ key_pos[p] if p in key_pos else model.to_string(example_prompt_ids[p]).strip() for p in posiciones_relevantes ] # type: ignore
-
+        position = "na"
+    
     results[(layer, head)] = {
-        "tipo": tipo,
-        "entropy": entropy,
+        "type": tipo,
         "top1_frac": top1_frac,
         "top2_frac": top2_frac,
-        "posiciones_relevantes": rel_pos,
-        "valores_relevantes": valores_relevantes,
+        "position": position,
     }
+
   return results
 
 def calculate_entropy(val_components: Float[Tensor, "seq"]):
